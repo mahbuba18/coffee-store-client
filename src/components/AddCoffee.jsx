cec93ba49507dc90b8457d5a7f1097d2e3a52bda
Mahbuba Islam Priya <mahbubacse18@gmail.com>
2025-02-27 23:38:57 +0600

    first commit

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,12 @@
+
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h3>Add Coffee</h3>
+            
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,12 @@
+
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h3>Update Coffee</h3>
+            
+        </div>
+    );
+};
+
+export default UpdateCoffee;
